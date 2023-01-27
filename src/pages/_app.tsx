7e4e7d08eb2da1fafa75297d5ae8816619288d8a
OlehDaschuk <o.v.dashchuk.fitis21@chdtu.edu.ua>
2023-01-27 14:32:55 +0200

    migrated from redux to mobx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
-import { Provider } from 'react-redux';
-
-import store from '@/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextNProgress />
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <Component {...pageProps} />
     </>
   );
 }
