80917df8d5819db8bfab2ffc02d835e8b90bae5d
OlehDaschuk <o.v.dashchuk.fitis21@chdtu.edu.ua>
2023-02-01 14:41:15 +0200

    added CopyToClipboardButton component

---
+++ src/components/shared/CopyToClipboardButton.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Button, Snackbar } from '@mui/material';
+import { SxProps } from '@mui/material';
+
+interface IProps {
+  sx?: SxProps;
+  copyText: string;
+  children: string;
+}
+
+export const CopyToClipboardButton: React.FC<IProps> = ({ sx, copyText, children }) => {
+  const [open, setOpen] = useState(false);
+  // ?: should i wrap in useCallback
+  const handleClick = () => {
+    setOpen(true);
+    navigator.clipboard.writeText(window.location.toString());
+  };
+  return (
+    <>
+      <Button fullWidth variant="contained" sx={{ ...sx }} onClick={handleClick}>
+        {children}
+      </Button>
+
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={open}
+        onClose={() => setOpen(false)}
+        autoHideDuration={3000}
+        message="Copied to clipboard"></Snackbar>
+    </>
+  );
+};
